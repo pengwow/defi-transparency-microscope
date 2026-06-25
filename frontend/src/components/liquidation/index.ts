86bec63b735718c89403800f279c4af2e05e9ecb
TDD Subagent <tdd-subagent@dtm.local>
2026-06-25 17:24:55 +0000

    feat(frontend): add liquidation/index.ts barrel export

---
+++ frontend/src/components/liquidation/index.ts
@@ -0,0 +1,54 @@
+/**
+ * Barrel export for the `liquidation` components.
+ *
+ * Usage:
+ *   import { PanoramaView, FocusView } from '@/components/liquidation';
+ */
+
+export { PanoramaView } from './PanoramaView';
+export type { PanoramaViewProps } from './PanoramaView';
+
+export { FocusView } from './FocusView';
+export type { FocusViewProps } from './FocusView';
+
+export { HeatmapPanel } from './HeatmapPanel';
+export type { HeatmapPanelProps } from './HeatmapPanel';
+
+export { PendingMempool } from './PendingMempool';
+export type { PendingMempoolProps } from './PendingMempool';
+
+export { AmmDisturbanceMap } from './AmmDisturbanceMap';
+export type { AmmDisturbanceMapProps } from './AmmDisturbanceMap';
+
+export { ProtocolStats } from './ProtocolStats';
+export type { ProtocolStatsProps } from './ProtocolStats';
+
+export { AddressInput } from './AddressInput';
+export type { AddressInputProps } from './AddressInput';
+
+export { SimParams } from './SimParams';
+export type { SimParamsProps } from './SimParams';
+
+export { ExperimentControls } from './ExperimentControls';
+export type { ExperimentControlsProps } from './ExperimentControls';
+
+export { RedAlert } from './RedAlert';
+export type { RedAlertProps } from './RedAlert';
+
+export { HfGaugePanel } from './HfGaugePanel';
+export type { HfGaugePanelProps } from './HfGaugePanel';
+
+export { PriceHfCurve } from './PriceHfCurve';
+export type { PriceHfCurveProps } from './PriceHfCurve';
+
+export { LiquidationTimeline } from './LiquidationTimeline';
+export type { LiquidationTimelineProps } from './LiquidationTimeline';
+
+export { AttributionPanel } from './AttributionPanel';
+export type { AttributionPanelProps } from './AttributionPanel';
+
+export { PositionDetails } from './PositionDetails';
+export type { PositionDetailsProps } from './PositionDetails';
+
+export { LiquidationExplanation } from './LiquidationExplanation';
+export type { LiquidationExplanationProps } from './LiquidationExplanation';
